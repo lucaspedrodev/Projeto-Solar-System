0d6fd0a39e610c814cceec5ed767b0b7831bf9ff
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-19 11:33:20 -0300

    requisito 09

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 import PlanetCard from './components/PlanetCard';
 import Missions from './components/Missions';
+import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    const missao = {
+      name: 'Galileo',
+      year: '1989',
+      country: 'USA',
+      destination: 'Júpter',
+    };
     return (
       <section>
         <div data-testid="missions"> </div>
         <Title headline="Missões" />
+        <MissionCard missao={ missao } />
       </section>
     );
   }
