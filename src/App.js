881b84e5a02b464b07aaabd57e10e541e538683c
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-19 10:05:24 -0300

    requisito 07

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 import PlanetCard from './components/PlanetCard';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         </div>
         <div>
           <SolarSystem />
+          <Missions />
         </div>
         <div>
           <Title headline="" />
